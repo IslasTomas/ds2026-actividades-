c49cbb867ed9444b97f0170409df87bfb5390662
Capitan <capitan@MBP-de-Capitan.fibertel.com.ar>
2026-04-19 21:30:47 -0300

    c03 - ejercicio 1 - Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,11 @@
+const nombre = "Tomas Islas";
+const edad = 31;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+contador += 1;
+contador += 1;
+contador += 1;
+console.log(contador);
